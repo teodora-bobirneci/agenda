5ca63459a6488d8e2750c7ab08a81272fa0dfeaf
Teodora <teobobirneci@yahoo.com>
2017-11-23 23:00:32 +0200

    wip

--- src/main/java/org/learn/agenda/Application.java
+++ src/main/java/org/learn/agenda/Application.java
@@ -1,13 +1,23 @@
 package org.learn.agenda;
 
+import org.learn.agenda.security.ApplicationSecurity;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 /**
  * @author Teodora Bobirneci
  */
 @SpringBootApplication
 public class Application {
+
+    @Bean
+    public WebSecurityConfigurerAdapter webSecurityConfigurerAdapter() {
+        return new ApplicationSecurity();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
