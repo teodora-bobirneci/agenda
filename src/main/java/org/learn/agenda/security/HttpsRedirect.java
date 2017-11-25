11847edfb5bcff1cc32ea2fe22c46131a908323e
Teodora <teobobirneci@yahoo.com>
2017-11-25 20:54:37 +0200

    working https and demo authentication

---
+++ src/main/java/org/learn/agenda/security/HttpsRedirect.java
@@ -0,0 +1,32 @@
+package org.learn.agenda.security;
+
+import org.apache.catalina.connector.Connector;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author Teodora Bobirneci
+ */
+@Configuration
+public class HttpsRedirect {
+
+    @Bean
+    public EmbeddedServletContainerFactory servletContainer() {
+        TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory();
+        tomcat.addAdditionalTomcatConnectors(redirectConnector());
+        return tomcat;
+    }
+
+    private Connector redirectConnector() {
+        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+        connector.setScheme("http");
+        connector.setPort(8080);
+        connector.setSecure(false);
+        connector.setRedirectPort(8443);
+
+        return connector;
+    }
+
+}
---
+++ src/main/java/org/learn/agenda/security/SecurityConfig.java
@@ -0,0 +1,31 @@
+package org.learn.agenda.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @author Teodora Bobirneci
+ */
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    //    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http.authorizeRequests()
+//                .antMatchers("/css/**", "/index").permitAll()
+//                .antMatchers("/user/**").hasRole("USER")
+//                .and()
+//                .formLogin()
+//                .loginPage("/login").failureUrl("/login-error");
+//    }
+//
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("user").password("password").roles("USER");
+    }
+}
